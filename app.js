c854ac379f75c87d41b475969b43294c236d933e
shakibo <shakib.shaon@softbdltd.com>
2021-08-11 16:39:03 +0600

    note adding code is on progress

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 
 const chalk = require('chalk');
 const yargs = require('yargs');
-const getNotes = require('./notes');
+const notes = require('./notes');
 
 
 //yargs version change
@@ -25,8 +25,7 @@ yargs.command({
         }
     },
     handler: function (argv) {
-        console.log('Title: ' + argv.title);
-        console.log('Body: ' + argv.body);
+       notes.addNote(argv.title, argv.body)
     }
 })
 
--- notes.js
+++ notes.js
@@ -1,5 +1,42 @@
+const fs = require('fs')
 const getNotes = function () {
     return 'Your note...'
 }
 
-module.exports = getNotes;
+const addNote = function (title, body) {
+    const notes = loadNotes()
+    const duplicateNotes = notes.filter(function (note) {
+        return note.title === title
+    })
+
+    if (note.title.length === 0) {
+        notes.push({
+            title: title,
+            body: body
+        })
+        saveNotes(notes)
+
+        console.log('New note added!');
+    }
+
+}
+
+const saveNotes = function (notes) {
+    const dataJSON = JSON.stringify(notes)
+    fs.writeFileSync('notes.json', dataJSON)
+}
+
+const loadNotes = function () {
+    try{
+        const noteBuffer = fs.readFileSync('notes.json')
+        const noteJSON = noteBuffer.toString()
+        return JSON.parse(noteJSON)
+    } catch (err) {
+        return []
+    }
+}
+
+module.exports = {
+    getNotes: getNotes,
+    addNote: addNote,
+};
